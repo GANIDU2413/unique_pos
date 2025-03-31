ef9061f37333a60c71cb461d8b0067b3ab51c498
GANIDU2413 <ganidusahan@gmail.com>
2025-03-31 21:11:25 +0530

    add new files and new dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:unique_pos/register_screen.dart';
-import 'login_screen.dart';
-import 'product_screen.dart';
-import 'product_provider.dart';
+import 'screens/login_screen.dart';
+import 'services/database_service.dart';
+import 'models/auth_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  await DatabaseService().initializeDatabase();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthModel()),
+      ],
+      child: UniqueSportsApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class UniqueSportsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ProductProvider()),
-      ],
-      child: MaterialApp(
-        title: 'POS System',
-        initialRoute: '/',
-        routes: {
-          '/': (context) => LoginScreen(),
-          '/home': (context) => ProductScreen(),
-          '/register': (context) => RegisterScreen(), // Add registration route
-        },
+    return MaterialApp(
+      title: 'Unique Sports POS',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: LoginScreen(),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import open_file_mac
 import path_provider_foundation
-import printing
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
